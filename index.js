37558fc097558704749973652208da84abbe33cc
Лаверов Денис <denis.laverov@corezoid.com>
2019-01-24 16:16:38 +0200

    1

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 
+const axios = require("axios");
 
 
 
 class HelloWorld{
-   constructor(name){
-     this.name = name;
+   constructor(url){
+     this.url = url;
+   }
+
+   async getScheme(obj_id, obj_type){
+      return axios.post(this.url, {"ops":[{"type":"get", "obj":"scheme", obj_id, obj_type}]})
    }
 
    getMe(){
